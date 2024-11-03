4765b555ab8cc99300ae70738971a30dcde79d62
yassinxx <yassin.ela@hotmail.de>
2024-11-03 21:52:42 +0100

    Added first Spring Boot Project

---
+++ projectx/src/main/java/edu/fra/uas/ProjectxApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectxApplication.class, args);
+	}
+
+}
---
+++ projectx/src/main/java/edu/fra/uas/projectx/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package edu.fra.uas.projectx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value="hello", method=RequestMethod.GET)
+    @ResponseBody
+    public String getHelloWorld() {
+        return "<h1>Hello World Spring Boot!</h1>";
+    }
+}
